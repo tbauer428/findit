5bcdd97ed821d9e367e8991ce9cb060e0e8f7dc4
Thomas <tom.bauer95@gmail.com>
2020-07-23 22:54:13 -0500

    moved line inspection funcitonality into its own unit and added some color

---
+++ src/lines.rs
@@ -0,0 +1,31 @@
+use indicatif::ProgressBar;
+use crate::ResultSet;
+
+pub fn parse_lines(pattern: String, pb: &ProgressBar, result_set: &mut ResultSet, _content: String) {
+
+    let mut line_count = 0;
+
+    for line in _content.lines() {
+        if line.contains(&pattern) {
+
+            line_count = line_count + 1;
+
+            let mut new_vec = vec![];
+
+            new_vec.push(line.parse().unwrap());
+
+            new_vec.push(format!("{}", line_count));
+
+            result_set.result_matches.push(new_vec);
+
+            pb.inc(1);
+
+        }else {
+
+            line_count = line_count + 1;
+
+            pb.inc(1);
+
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,80 @@
+mod lines;
+
 use structopt::StructOpt;
 use indicatif::ProgressBar;
+use console::style;
 
 #[derive(StructOpt)]
 struct Cli {
-    #[structopt(help = "You can pass in patterns as just a word, or use a phrase by surrounding your argument with single quotes 'like this'!")]
+    #[structopt(help = "You can pass in patterns as just a word, \
+    or use a phrase by surrounding your argument with single quotes 'like this'!")]
     pattern: String,
-    #[structopt(parse(from_os_str), help = "Here you can pass the path to *almost* any file that's readable as text.")]
+    #[structopt(parse(from_os_str), help = "Here you can pass the path to *almost* \
+     any file that's readable as text.")]
     path: std::path::PathBuf,
 }
 
-struct ResultSet {
+pub struct ResultSet {
     result_matches: Vec<Vec<String>>
 }
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+    use crate::lines::parse_lines;
 
-    let args = Cli::from_args();
+    let from_args = Cli::from_args();
 
-    let result = std::fs::read_to_string(&args.path);
+    let result = std::fs::read_to_string(&from_args.path);
 
     let _content = match result {
         Ok(content) => { content }
         Err(error) => { return Err(error.into()); }
     };
 
-    let pb = ProgressBar::new(_content.lines().count() as u64);
+    let pb = &ProgressBar::new(_content.lines().count() as u64);
 
-    let mut result_set = ResultSet { result_matches: vec![] };
+    let result_set = &mut ResultSet { result_matches: vec![] };
 
-    let mut line_count = 0;
+    let pattern = format!("{}", &from_args.pattern);
 
-    for line in _content.lines() {
-        if line.contains(&args.pattern) {
+    let line_count = _content.lines().enumerate().count();
 
-            line_count = line_count + 1;
+    parse_lines(pattern, pb, result_set, _content);
 
-            let mut new_vec = vec![];
+    pb.finish_and_clear();
 
-            new_vec.push(line.parse().unwrap());
+    let result_or_results =
+        if *&result_set.result_matches.len() as u32 == 1 { "result" } else { "results" };
 
-            new_vec.push(format!("{}", line_count));
-
-            result_set.result_matches.push(new_vec);
-
-            pb.inc(1);
-
-        }else {
-            line_count = line_count + 1;
-
-            pb.inc(1);
-
-        }
-    }
-
-    pb.finish();
-
-    let result_or_results = if *&result_set.result_matches.len() as u32 == 1 { "result" } else { "results" };
 
     println!("Found {} matching {} in {} total lines...",
-             &result_set.result_matches.len(),
+             style(&result_set.result_matches.len()).bold().green(),
              result_or_results,
              format!("{}", line_count));
+    println!();
+
     for result in &result_set.result_matches {
 
-            println!("{}: {}", result[1], result[0])
+            println!("{}: {}", style(result[1].to_string()).cyan(), result[0])
 
     }
 
     Ok(())
+
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test() {
+
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_any_panic() {
+        panic!("Ahh! Watch those wrist rockets!")
+    }
 
 }
