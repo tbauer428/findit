b18af326f7649658750b3525323d356320b447da
Thomas <tom.bauer95@gmail.com>
2021-03-10 21:16:25 -0600

    formatting because I couldn't stand to look at it anymore

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 mod lines;
 
-use structopt::StructOpt;
-use indicatif::ProgressBar;
 use console::style;
+use indicatif::ProgressBar;
+use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
     #[structopt(help = "You can pass in patterns as just a word, \
     or use a phrase by surrounding your argument with single quotes 'like this'!")]
     pattern: String,
-    #[structopt(parse(from_os_str), help = "Here you can pass the path to *almost* \
-     any file that's readable as text.")]
+    #[structopt(
+        parse(from_os_str),
+        help = "Here you can pass the path to *almost* \
+     any file that's readable as text."
+    )]
     path: std::path::PathBuf,
 }
 
 pub struct ResultSet {
-    result_matches: Vec<Vec<String>>
+    result_matches: Vec<Vec<String>>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     use crate::lines::parse_lines;
 
     let from_args = Cli::from_args();
@@ -26,13 +29,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let result = std::fs::read_to_string(&from_args.path);
 
     let _content = match result {
-        Ok(content) => { content }
-        Err(error) => { return Err(error.into()); }
+        Ok(content) => content,
+        Err(error) => {
+            return Err(error.into());
+        }
     };
 
     let pb = &ProgressBar::new(_content.lines().count() as u64);
 
-    let result_set = &mut ResultSet { result_matches: vec![] };
+    let result_set = &mut ResultSet {
+        result_matches: vec![],
+    };
 
     let pattern = format!("{}", &from_args.pattern);
 
@@ -42,24 +49,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     pb.finish_and_clear();
 
-    let result_or_results =
-        if *&result_set.result_matches.len() as u32 == 1 { "result" } else { "results" };
+    let result_or_results = if *&result_set.result_matches.len() as u32 == 1 {
+        "result"
+    } else {
+        "results"
+    };
 
-
-    println!("Found {} matching {} in {} total lines...",
-             style(&result_set.result_matches.len()).bold().green(),
-             result_or_results,
-             format!("{}", line_count));
+    println!(
+        "Found {} matching {} in {} total lines...",
+        style(&result_set.result_matches.len()).bold().green(),
+        result_or_results,
+        format!("{}", line_count)
+    );
     println!();
 
     for result in &result_set.result_matches {
-
-            println!("{}: {}", style(result[1].to_string()).cyan(), result[0])
-
+        println!("{}: {}", style(result[1].to_string()).cyan(), result[0])
     }
 
     Ok(())
-
 }
 
 #[cfg(test)]
@@ -67,14 +75,11 @@ mod tests {
     // use super::*;
 
     #[test]
-    fn test() {
-
-    }
+    fn test() {}
 
     #[test]
     #[should_panic]
     fn test_any_panic() {
         panic!("Ahh! Watch those wrist rockets!")
     }
-
 }
