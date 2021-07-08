4f00e4296d2df035a13235f0b35a23d11c95fda5
Thomas Bauer <tom.bauer95@gmail.com>
2021-07-07 23:39:36 -0500

    saving results to file with flag

--- src/lines.rs
+++ src/lines.rs
@@ -1,13 +1,16 @@
-use indicatif::ProgressBar;
 use crate::ResultSet;
+use indicatif::ProgressBar;
 
-pub fn parse_lines(pattern: String, pb: &ProgressBar, result_set: &mut ResultSet, _content: String) {
-
+pub fn parse_lines(
+    pattern: String,
+    pb: &ProgressBar,
+    result_set: &mut ResultSet,
+    _content: String,
+) {
     let mut line_count = 0;
 
     for line in _content.lines() {
         if line.contains(&pattern) {
-
             line_count = line_count + 1;
 
             let mut new_vec = vec![];
@@ -19,13 +22,10 @@ pub fn parse_lines(pattern: String, pb: &ProgressBar, result_set: &mut ResultSet
             result_set.result_matches.push(new_vec);
 
             pb.inc(1);
-
-        }else {
-
+        } else {
             line_count = line_count + 1;
 
             pb.inc(1);
-
         }
     }
 }
